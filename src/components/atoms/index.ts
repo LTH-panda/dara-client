e2a6fa0a8d2bf8a8af7f0c2243cbc25bfbebecbb
LTH <johtta888@naver.com>
2022-08-03 02:19:56 +0900

    Add: 추가 SvgIcon 컴포넌트

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from "./Button";
 export { default as Dropdown } from "./Dropdown";
 export { default as LinkTo } from "./LinkTo";
 export { default as Logotypo } from "./Logotypo";
+export { default as SvgIcon } from "./SvgIcon";
