bc0249652d276f16877ce658ae544d824d4d6365
LTH <johtta888@naver.com>
2022-08-05 03:34:59 +0900

    ADD: 추가 useClickOutside

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,20 @@
+import { RefObject, useEffect } from "react";
+
+function useClickOutside(ref: RefObject<T>, hanlder: (e: Event) => void) {
+  useEffect(() => {
+    const listener = (e: Event) => {
+      if (!ref.current || ref.current.contains(e.target)) return;
+
+      hanlder(e);
+    };
+    document.addEventListener("mousedown", listener);
+    document.addEventListener("touchstart", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+      document.removeEventListener("touchstart", listener);
+    };
+  }, [ref, hanlder]);
+}
+
+export default useClickOutside;
