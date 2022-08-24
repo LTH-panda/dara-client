402af83bc678de86d3dface018155e0ebe4b2fa1
LTH <johtta888@naver.com>
2022-08-24 15:27:24 +0900

    Add: 추가 Header style, logic

--- src/components/modules/Header/index.tsx
+++ src/components/modules/Header/index.tsx
@@ -1,4 +1,5 @@
-import { LinkTo, Logotypo } from "components/atoms";
+import { LinkTo, Logotypo, Spinner } from "components/atoms";
+import { useSession } from "next-auth/react";
 import React from "react";
 import HeaderModal from "../HeaderModal";
 import * as S from "./style";
@@ -9,14 +10,22 @@ type HeaderProps = {
 };
 
 function Header({ search, onChange }: HeaderProps) {
+  const { status } = useSession();
+
   return (
     <S.Header>
       <Logotypo />
       <S.SearchBar placeholder="검색" value={search} onChange={onChange} />
       <S.Nav>
-        <LinkTo href="/">완료 영상</LinkTo>
-        <LinkTo href="/">의뢰 영상</LinkTo>
-        <HeaderModal name="아무개" email="email@email.com" />
+        <LinkTo href="/complete">완료 영상</LinkTo>
+        <LinkTo href="/commission">의뢰 영상</LinkTo>
+        {status === "loading" ? (
+          <Spinner isVisible />
+        ) : status === "authenticated" ? (
+          <HeaderModal name="아무개" email="email@email.com" />
+        ) : (
+          <LinkTo href="/auth">로그인</LinkTo>
+        )}
       </S.Nav>
     </S.Header>
   );
--- src/components/modules/Header/style.ts
+++ src/components/modules/Header/style.ts
@@ -1,14 +1,23 @@
 import styled from "@emotion/styled";
+import theme from "commons/styles/theme";
 
 export const Header = styled.header`
   display: flex;
   align-items: center;
   gap: 2rem;
-  padding: 2rem;
+  padding: 1rem 2rem;
   height: 6.25rem;
 `;
 
-export const SearchBar = styled.input``;
+export const SearchBar = styled.input`
+  display: flex;
+  align-items: center;
+  padding: 1rem 2rem;
+  width: 40%;
+  border: 1px solid #b3b3b3;
+  border-radius: 40px;
+  outline-color: ${theme.color.primary};
+`;
 
 export const Nav = styled.nav`
   display: flex;
